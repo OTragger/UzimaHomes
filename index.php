5afcf2e86bde722d7996049bf66f9d8c8cd466b2
Oswald Houndekon <oswaldtragger@gmail.com>
2022-11-25 21:47:20 +0000

    first commit, creted the page, created and added the logo files

---
+++ index.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uzima Homes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<img src="assets/img/uzima-3d.png" alt="" class="uzima-3d">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <a class="navbar-brand d-lg-none ms-3" href="#">
+    <div class="d-flex flex-row justify-content-center align-items-center">
+        <img class="d-lg-none" width="30" src="assets/img/uzima-logo.svg" alt="">
+        <span class="log d-none d-md-flex ms-1 fs-3">UZIMA HOMES</span>
+    </div>
+  </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      <div class="navbar-nav align-items-center">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Guides</a>
+        <a class="navbar-brand d-none d-lg-flex mx-3" href="#"><img width="40" src="assets/img/uzima-logo.svg" alt=""></a>
+        <a class="nav-link" href="#">Flights</a>
+        <a class="nav-link" href="#">About</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<section id="hero" class="container-fluid py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center text-md-start py-5">
+                <p class="display-1 fw-bold">
+                    Fine 2/3 bedroom apartments
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, alias expedita accusantium quae illum commodi repellat voluptate praesentium. Accusamus a suscipit nostrum error autem rem ex quas esse magnam voluptate.</p>
+                <p class="btn btn-dark px-5">Dive in</p>
+            </div>
+            <div class="col d-none d-md-flex"></div>
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid py-5" id="book-appointment">
+    <div class="container px-5 text-center text-light d-flex flex-column align-items-center">
+        <p class="fs-1 fw-bold">
+            Book an appointment
+        </p>
+        <p class="w-75 d-lg-none">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas accusantium, ad provident quod blanditiis molestias distinctio ipsam aperiam eum dolores ab, laudantium iste deserunt a asperiores quisquam enim at maxime!</p>
+        <p class="w-50 d-none d-lg-flex">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas accusantium, ad provident quod blanditiis molestias distinctio ipsam aperiam eum dolores ab, laudantium iste deserunt a asperiores quisquam enim at maxime!</p>
+        <p class="btn btn-outline-light px-5">Here</p>
+    </div>
+</section>
+
+<section class="container-fluid py-5" id="presentation">
+    <div class="container py-5">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 p-3 col-md-4">
+                <div class="d-flex flex-md-column align-items-end">
+                    <div class="d-img w-100"></div>
+                    <div class="ps-3 ps-md-0">
+                        <hr class="w-25">
+                        <h4 class="fw-bold">Lorem ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam tempora voluptas quaerat id alias excepturi commodi, enim quibusdam inventore magnam ipsum reprehenderit vitae! Dolores deleniti mollitia obcaecati illum sed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 p-3 col-md-4">
+                <div class="d-flex flex-md-column align-items-end">
+                    <div class="d-img w-100"></div>
+                    <div class="ps-3 ps-md-0">
+                        <hr class="w-25">
+                        <h4 class="fw-bold">Lorem ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam tempora voluptas quaerat id alias excepturi commodi, enim quibusdam inventore magnam ipsum reprehenderit vitae! Dolores deleniti mollitia obcaecati illum sed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 p-3 col-md-4">
+                <div class="d-flex flex-md-column align-items-end">
+                    <div class="d-img w-100"></div>
+                    <div class="ps-3 ps-md-0">
+                        <hr class="w-25">
+                        <h4 class="fw-bold">Lorem ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam tempora voluptas quaerat id alias excepturi commodi, enim quibusdam inventore magnam ipsum reprehenderit vitae! Dolores deleniti mollitia obcaecati illum sed.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="four" class="container-fluid py-5">
+    <div class="container py-5 text-center d-flex flex-column align-items-center">
+        <img src="assets/img/uzima-logo.svg" width="50" alt="">
+        <p class="display-3 fw-bold w-75">Learn more about our apartments</p>
+        <p class="w-75 d-lg-none">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas accusantium, ad provident quod blanditiis molestias distinctio ipsam aperiam eum dolores ab, laudantium iste deserunt a asperiores quisquam enim at maxime!</p>
+        <p class="w-50 d-none d-lg-flex">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas accusantium, ad provident quod blanditiis molestias distinctio ipsam aperiam eum dolores ab, laudantium iste deserunt a asperiores quisquam enim at maxime!</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="d-none d-lg-flex col-lg-6 d-flex flex-row justfy-content-end">
+                <img class="image-left" src="./assets/img/WhatsApp Image 2022-11-16 at 10.46.06.jpeg" alt="">
+            </div>
+            <div class="col col-lg-6">
+                <img class="img-fluid pb-3" src="./assets/img/WhatsApp Image 2022-11-16 at 10.46.07 (1).jpeg" alt="">
+                <div class="row">
+                    <div class="col">
+                        <p class="h3">Welcome to the battle royale</p>
+                    </div>
+                    <div class="col">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur tenetur temporibus corporis ipsa voluptatum laboriosam tempore volu adipisci enim?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur tenetur temporibus corporis ipsa voluptatum laboriosam tempore volu adipisci enim?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="five" class="container-fluid px-0 py-5 text-light">
+    <div class="container ms-3 me-0 py-5 px-0 d-flex flex-column-reverse flex-md-row">
+        <div class="p-3">
+            <p class="display-6 fw-bold">Choose apartments that fit your needs</p>
+            <div class="mb-3">
+                <p class="h3">Lorem ipsum dolor</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sunt perspiciatis obcaecati quaerat, incidunt doloribus totam veritatis nostrum quisquam optio corporis ea iure est. Minus molestiae sapiente odit harum perspiciatis.</p>
+            </div>
+            <div class="mb-3">
+                <p class="h3">Lorem ipsum dolor</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sunt perspiciatis obcaecati quaerat, incidunt doloribus totam veritatis nostrum quisquam optio corporis ea iure est. Minus molestiae sapiente odit harum perspiciatis.</p>
+            </div>
+        </div>
+        <div class="d-none d-lg-flex col-lg-6 d-flex flex-row justfy-content-end">
+            <img class="image-left" src="./assets/img/WhatsApp Image 2022-11-16 at 10.46.06.jpeg" alt="">
+        </div>
+    </div>
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
